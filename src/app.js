2ee19b0f4d2d648909d5fd8d738bc1f922a153de
Harry Gustave <harry23gust@gmail.com>
2021-04-15 21:21:57 -0500

    added code to copy file in new dist folder, also destructured code

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
-const fs = require('fs');
+//const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
+
 const inquirer = require('inquirer');
 const generatePage = require('./page-template.js');
 
@@ -132,23 +134,20 @@ Add a New Project
 
 
 promptUser()
-  .then(promptProject)
-  .then(portfolioData => {
-     const pageHTML = generatePage(portfolioData);
-
-     fs.writeFile('./dist/index.html', pageHTML, err => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      console.log('Page created! Check out index.html in this directory to see it!');
-    
-      fs.copyFile('./style.css', './dist/style.css', err => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log('Style sheet copied successfully!');
-      });
-    });
-  });
+.then(promptProject)
+.then(portfolioData => {
+  return generatePage(portfolioData);
+})
+.then(pageHTML => {
+  return writeFile(pageHTML);
+})
+.then(writeFileResponse => {
+  console.log(writeFileResponse);
+  return copyFile();
+})
+.then(copyFileResponse => {
+  console.log(copyFileResponse);
+})
+.catch(err => {
+  console.log(err);
+});
---
+++ src/utils/generate-site.js
@@ -0,0 +1,37 @@
+// writing files
+
+const fs = require('fs');
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+      fs.writeFile('./dist/index.html', fileContent, err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve({
+          ok: true,
+          message: 'File created!'
+        });
+      });
+    });
+  };
+  
+  // copying file
+  const copyFile = () => {
+    return new Promise((resolve, reject) => {
+      fs.copyFile('./src/style.css', './src/dist/style.css', err => {
+        if (err) {
+          reject(err);
+          return;
+        }
+  
+        resolve({
+          ok: true,
+          message: 'Stylesheet created!'
+        });
+      });
+    });
+  };
+  
+  module.exports = { writeFile, copyFile };
